c971adcee6d8a9b3daf7494b5809cb8f6ea02a2a
Killian Herrera Garcia <41011561p@iespoblenou.org>
2019-03-07 03:04:58 +0100

    Añadido test para podificar "toolbar" y "navigation drawer" dependiendo de la ruta actual.

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,87 @@
 <template>
-  <v-app>
-    <v-navigation-drawer absolute v-model="sideNav">
-      <v-toolbar flat>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">
-              <router-link to="/" tag="soan" style="cursor: pointer"><i class="ss ss-2x ss-mor"></i>Home</router-link>
-            </v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
+    <v-app>
+        <v-navigation-drawer absolute v-model="sideNav">
+            <v-toolbar flat>
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title class="title">
+                            <router-link to="/" tag="soan" style="cursor: pointer"><i class="ss ss-2x ss-mor"></i>Home
+                            </router-link>
+                        </v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-toolbar>
 
-      <v-divider></v-divider>
-      <v-list>
-        <v-list-tile
-                v-for="item in menuItems"
-                :key="item.title"
-                router
-                :to="item.link">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar height="50" class="light-blue lighten-1">
-      <v-toolbar-title>
-        <router-link to="/" tag="soan" style="cursor: pointer"><i class="ss ss-2x ss-mor"></i>Home</router-link>
-      </v-toolbar-title>
-      <v-toolbar-side-icon
-              @click.native.stop="sideNav = !sideNav"
-              class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <spacer></spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn
-                flat
-                v-for="item in menuItems"
-                :key="item.title"
-                router
-                :to="item.link">
-          <i :class="'ss ss-2x ss-'+item.icon"></i>
-          &nbsp;
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <main>
-      <router-view></router-view>
-    </main>
-  </v-app>
+            <v-divider></v-divider>
+            <v-list>
+                <v-list-tile
+                        v-for="item in menuItems"
+                        :key="item.title"
+                        router
+                        :to="item.link">
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar height="50" class="light-blue lighten-1">
+            <v-toolbar-title>
+                <router-link to="/" tag="soan" style="cursor: pointer"><i class="ss ss-2x ss-mor"></i>Home</router-link>
+            </v-toolbar-title>
+            <v-toolbar-side-icon
+                    @click.native.stop="sideNav = !sideNav"
+                    class="hidden-sm-and-up"></v-toolbar-side-icon>
+            <spacer></spacer>
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn
+                        flat
+                        v-for="item in menuItems"
+                        :key="item.title"
+                        router
+                        :to="item.link">
+                    <i :class="'ss ss-2x ss-'+item.icon"></i>
+                    &nbsp;
+                    {{ item.title }}
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <main>
+            <router-view></router-view>
+        </main>
+    </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        sideNav: false,
-        iconMoon: 'ss ss-2x ss-drk'
-      }
-    },
-    computed: {
-      menuItems () {
-        let menuItems = [
-          { icon: '', title: 'Alumnos', link: '/alumnos' },
-          { icon: '', title: 'Demandas', link: '/demandas' },
-          { icon: '', title: 'Intervenciones', link: '/intervenciones' },
-          { icon: '', title: 'Nueva', link: '/nueva' },
-          { icon: '', title: 'Eliminar', link: '/eliminar' }
-        ]
-        return menuItems
-      }
+    export default {
+        data() {
+            return {
+                sideNav: false,
+                iconMoon: 'ss ss-2x ss-drk'
+            }
+        },
+        computed: {
+            menuItems() {
+                let menuItems = [
+                    {icon: '', title: 'Alumnos', link: '/alumnos'},
+                    {icon: '', title: 'Demandas', link: '/demandas'},
+                    {icon: '', title: 'Intervenciones', link: '/intervenciones'},
+                    {icon: '', title: 'Nueva', link: '/nueva'},
+                    {icon: '', title: 'Eliminar', link: '/eliminar'}
+                ]
+                if (this.$route.path == '/demandas') {
+                    menuItems = [
+                        {icon: '', title: 'Alumnos', link: '/alumnos'},
+                        {icon: '', title: 'TEST', link: '/demandas'},
+                        {icon: '', title: 'Intervenciones', link: '/intervenciones'},
+                        {icon: '', title: 'Nueva', link: '/nueva'},
+                        {icon: '', title: 'Eliminar', link: '/eliminar'}
+                    ]
+                }
+                return menuItems
+            }
+        }
     }
-  }
 </script>
 
